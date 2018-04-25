ec4e70eaaeedd777fb2d865907329457a8610766
sergiolpf <sergiolpf@gmail.com>
2018-04-25 22:23:38 +1200

    feat: constants :)

--- src/components/constants/Constants.js
+++ src/components/constants/Constants.js
@@ -1,3 +1,26 @@
 export const ItemTypes = {
     BOOK: 'book'
-}
+};
+
+export const ShelfTypes = {
+    currentlyReading: {
+        id: 'currentlyReading',
+        label: 'Currently Reading',
+        display: true
+    },
+    wantToRead: {
+        id: 'wantToRead',
+        label: 'Want to Read',
+        display: true
+    },
+    read: {
+        id: 'read',
+        label: 'Read',
+        display: true
+    },
+    none: {
+        id: 'none',
+        label: 'None',
+        display: false
+    }
+};
